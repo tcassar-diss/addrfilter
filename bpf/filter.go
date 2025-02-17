6d68fd9582d3716b3cd4e5553e50cf5adb3cf661
Tom Cassar <cassar.thomas.e@gmail.com>
2025-02-17 18:20:42 +0000

    Refactor file names in bpf/

--- bpf/filter.go
+++ bpf/filter.go
@@ -11,9 +11,37 @@ import (
 )
 
 var (
+	ErrInvalidSyscallNr       = errors.New("invalid syscall number")
+	ErrFilenameMarshalFailed  = errors.New("failed to marshal filename")
 	ErrBadLibcRange           = errors.New("nonsensical libc range given")
 	ErrWhitelistAlreadyExists = errors.New("whitelist already exists")
 )
+
+type Stats struct {
+	GetCurrentTaskFailed uint64
+	TPEntered            uint64
+	IgnorePID            uint64
+	ReadPIDFailed        uint64
+	LibcNotLoaded        uint64
+	StackDebugEmpty      uint64
+	GetStackFailed       uint64
+	CallsiteLibc         uint64
+	StackTooShort        uint64
+	NoRPMapping          uint64
+	RPNullAfterMap       uint64
+	FilenameTooLong      uint64
+	FindVMAFailed        uint64
+	NoBackingVMA         uint64
+	WhitelistMissing     uint64
+	SyscallBlocked       uint64
+	SendSignalFailed     uint64
+}
+
+type Stacktrace struct {
+	Stack        [32]uint64
+	FramesWalked int32
+	CallSite     uint64
+}
 
 // Filter is the userspace counterpart to the `addrfilter` bpf program.
 //
--- bpf/whitelist.go
+++ bpf/whitelist.go
@@ -1,40 +1,6 @@
 package bpf
 
-import (
-	"errors"
-	"fmt"
-)
-
-var (
-	ErrInvalidSyscallNr      = errors.New("invalid syscall number")
-	ErrFilenameMarshalFailed = errors.New("failed to marshal filename")
-)
-
-type Stats struct {
-	GetCurrentTaskFailed uint64
-	TPEntered            uint64
-	IgnorePID            uint64
-	ReadPIDFailed        uint64
-	LibcNotLoaded        uint64
-	StackDebugEmpty      uint64
-	GetStackFailed       uint64
-	CallsiteLibc         uint64
-	StackTooShort        uint64
-	NoRPMapping          uint64
-	RPNullAfterMap       uint64
-	FilenameTooLong      uint64
-	FindVMAFailed        uint64
-	NoBackingVMA         uint64
-	WhitelistMissing     uint64
-	SyscallBlocked       uint64
-	SendSignalFailed     uint64
-}
-
-type Stacktrace struct {
-	Stack        [32]uint64
-	FramesWalked int32
-	CallSite     uint64
-}
+import "fmt"
 
 // Whitelist associates a filename with a set of allowed syscall numbers
 type Whitelist struct {
