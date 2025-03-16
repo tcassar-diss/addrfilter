b06b95be7a5230cbd6afb37f5c446146380f0ba2
tom cassar <cassar.thomas.e@gmail.com>
2025-03-16 17:45:21 +0000

    Make plot script create a dir with relevant benchmark info
    - Copy raw stats file into dir
    - Create plot in dir
    - Stats output in dir

--- utils/plot-profile.py
+++ utils/plot-profile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import pathlib
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,15 +14,27 @@ def process_file(filepath):
     stats = df.agg(['mean', 'median', 'std', 'sem']).transpose()
     stats.columns = ['Mean', 'Median', 'Std Dev', 'SEM']
     
+    s = ""
     # Print the statistics for each stage
-    print("Statistics for each stage:")
+    s += "Statistics for each stage:\n"
     for stage in stats.index:
-        print(f"\nStage: {stage}")
-        print(f"Mean: {stats.loc[stage, 'Mean']:.2f} ns")
-        print(f"Median: {stats.loc[stage, 'Median']:.2f} ns")
-        print(f"Standard Deviation: {stats.loc[stage, 'Std Dev']:.2f} ns")
-        print(f"SEM: {stats.loc[stage, 'SEM']:.2f} ns")
-    
+        s += f"\nStage: {stage}\n"
+        s += f"Mean: {stats.loc[stage, 'Mean']:.2f} ns\n"
+        s += f"Median: {stats.loc[stage, 'Median']:.2f} ns\n"
+        s += f"Standard Deviation: {stats.loc[stage, 'Std Dev']:.2f} ns\n"
+        s += f"SEM: {stats.loc[stage, 'SEM']:.2f} ns\n"
+
+
+    outdir = pathlib.Path(os.getcwd()).joinpath("./out/")
+    pathlib.Path.mkdir(outdir, exist_ok=True)
+
+    with open(outdir.joinpath("stats.txt"), "w+") as f:
+        _ = f.write(s)
+
+    with open(outdir.joinpath("raw.csv"), "w+") as f:
+        with open(filepath) as r:
+            _ = f.write(r.read())
+
     # Create and save the plot
     plt.figure(figsize=(10, 6))
     plt.bar(stats.index, stats['Mean'], yerr=stats['SEM'], capsize=5, color='skyblue')
@@ -29,7 +42,7 @@ def process_file(filepath):
     plt.title('Execution Time per Stage with SEM Error Bars')
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
-    plt.savefig('prof.pdf', bbox_inches='tight')
+    plt.savefig('./out/prof.pdf', bbox_inches='tight')
     plt.close()
 
 if __name__ == "__main__":
