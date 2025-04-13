4b8d528167f1083f44b31d4efee2421cc3d85199
tom cassar <cassar.thomas.e@gmail.com>
2025-04-13 18:48:21 +0000

    Remove cobra stuff

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "github.com/tcassar-diss/addrfilter/cmd"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+
+	"github.com/tcassar-diss/addrfilter/bpf"
+	"github.com/tcassar-diss/addrfilter/frontend"
+	"go.uber.org/zap"
+)
 
 func main() {
-	cmd.Execute()
+	l, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("failed to get zap production logger: %v\n", err)
+	}
+
+	logger := l.Sugar()
+	defer logger.Sync()
+
+	if len(os.Args) < 3 {
+		log.Fatalf("expected (at least) two args: whitelist, and executable (+ args)\n")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt)
+
+	command := exec.CommandContext(ctx, os.Args[2], os.Args[3:]...)
+	command.Stdout = os.Stdout
+
+	go func() {
+		for range stopper {
+			cancel()
+
+			return
+		}
+	}()
+
+	if err := command.Start(); err != nil {
+		log.Fatalf("failed to launch %s%s: %v", os.Args[2], fmt.Sprintf(" %s", os.Args[3:]), err)
+	}
+
+	if err := frontend.Start(
+		logger,
+		int32(command.Process.Pid),
+		os.Args[1],
+		&frontend.StartCfg{
+			WarnMode: bpf.Warn,
+			Profile:  false,
+		},
+	); err != nil {
+		log.Fatalf("failed to start filter: %v\n", err)
+	}
+
+	if err := command.Wait(); err != nil {
+		if err.Error() != "signal: interrupt" {
+			log.Fatalf("failed waiting on the executable: %v", err)
+		}
+
+		logger.Warnw("command killed by SIGINT", "cmd", os.Args[2])
+	}
 }
