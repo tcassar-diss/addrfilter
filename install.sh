dac3c073de9ab3df6dd63df6e939dfa495fc5b7b
tom cassar <cassar.thomas.e@gmail.com>
2025-04-14 00:09:25 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+log() { echo -e "\033[1;34m[INFO]\033[0m $*"; }
+error() { echo -e "\033[1;31m[ERROR]\033[0m $*" >&2; }
+
+BINARY_NAME="addrfilter"
+BUILD_PATH="./bin/$BINARY_NAME"
+INSTALL_PATH="/usr/local/bin/$BINARY_NAME"
+
+log "Building $BINARY_NAME..."
+make
+
+log "Installing to $INSTALL_PATH..."
+sudo install -m 755 "$BUILD_PATH" "$INSTALL_PATH"
+
+log "Build + install complete. Test with:"
+echo "  sudo $BINARY_NAME --help ..."
