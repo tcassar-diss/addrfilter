459b338ae5daf54be1049827a0ba11edf91078af
tom cassar <cassar.thomas.e@gmail.com>
2025-05-01 12:06:19 +0000

    Rename linkage to linked

--- examples/linkage/getpid.h
+++
@@ -1,7 +0,0 @@
-#ifndef GETPID_H
-#define GETPID_H
-
-#include <unistd.h>
-
-pid_t wrap_getpid();
-#endif /* GETPID_H */
--- examples/linkage/printf.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRINTF_H
-#define PRINTF_H
-
-int wrap_printf(const char *__restrict__ format, ...);
-
-#endif /* PRINTF_H */
